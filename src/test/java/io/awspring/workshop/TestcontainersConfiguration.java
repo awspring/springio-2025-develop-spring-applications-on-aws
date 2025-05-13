f08f75863f4e7cbda3760d66d74ab0ba484f189e
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2025-05-14 01:25:01 +0200

    Add Localstack via Testcontainers

--- src/test/java/io/awspring/workshop/TestcontainersConfiguration.java
+++ src/test/java/io/awspring/workshop/TestcontainersConfiguration.java
@@ -1,8 +1,20 @@
 package io.awspring.workshop;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    @ServiceConnection
+    @Bean
+    LocalStackContainer localStackContainer() {
+        return new LocalStackContainer(DockerImageName.parse("localstack/localstack:4.4.0"))
+                .withCopyFileToContainer(MountableFile.forClasspathResource("init-localstack.sh", 0744), "/etc/localstack/init/ready.d/init-aws.sh");
+    }
 
 }
