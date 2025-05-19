7ad74aa10309d14223dcdfccd1169c09c53a652d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2025-05-19 22:32:35 +0200

    Polish

--- src/main/java/io/awspring/workshop/service/QueueManagingService.java
+++ src/main/java/io/awspring/workshop/service/QueueManagingService.java
@@ -1,9 +1,6 @@
 package io.awspring.workshop.service;
 
-
 import org.springframework.stereotype.Component;
-
-
 
 @Component
 public class QueueManagingService {
@@ -11,13 +8,12 @@ public class QueueManagingService {
    //private final HashMap<String, SqsMessageListenerContainer> containerCache = new HashMap<>();
 
     public String createAndStartListener(String queueName) {
-        //Create, start and save container.
+        // todo: create, start and save container.
        return null;
     }
 
-
     public String stopListener(String queueName) {
-        // stop listening
+        // todo: stop listening
         return null;
     }
 }
